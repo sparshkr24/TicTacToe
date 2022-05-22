aef660456b8de519dbbf75d98b1861cdca900525
Sparsh Kumar <76520495+sparshkr24@users.noreply.github.com>
2022-05-23 01:01:50 +0530

    Added Board and Square components

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import React from "react";
+import Board from "./Components/Board/Board";
 
 const App = () => {
   return (
     <>
-      <h1>Welcome to React Vite Micro App!</h1>
-      <p>Hard to get more minimal than this React app.</p>
+      <h1>Tic Tac Toe</h1>
+      <Board />
     </>
   );
 };
---
+++ src/Components/Board/Square.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function Square(props) {
+  return <button type="button">{props.value}</button>;
+}
