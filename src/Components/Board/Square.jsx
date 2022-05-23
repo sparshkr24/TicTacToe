f5252f09639ab1c30d792b1ab2f539eaaf2e3915
Sparsh Kumar <76520495+sparshkr24@users.noreply.github.com>
2022-05-23 22:23:29 +0530

    Added X, O functionality

--- src/Components/Board/Square.jsx
+++ src/Components/Board/Square.jsx
@@ -1,5 +1,8 @@
 import React from "react";
 
+
 export default function Square(props) {
-  return <button className="square" type="button">{props.value}</button>;
+  return (
+    <button onClick = {props.onclick} className="square" type="button">{props.value}</button>
+  );
 }
