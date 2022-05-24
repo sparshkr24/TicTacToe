85d448262e13bc7df7f1cd558e306c07a41e70bc
Sparsh Kumar <76520495+sparshkr24@users.noreply.github.com>
2022-05-25 00:40:30 +0530

    added history

--- src/App.jsx
+++ src/App.jsx
@@ -1,46 +1,60 @@
 import React, {useState} from "react";
 import Board from "./Components/Board/Board";
 import {calculateWinner} from "./calculateWinner"
+import History from './Components/History/History'
 
 const App = () => {
 
   // state of board
-  const [board, setBoard] = useState(Array(9).fill(null)); 
-  // whose chance is next?
-  const [isXNext, setisXNext] = useState(true);
+  const [history, setHistory] = useState([
+    { board : Array(9).fill(null), isXNext: true }
+  ]); 
+
+  const[currentMove, setCurrentMove] = useState(0);
+  let current = history[currentMove];
 
   const handleBoardClick = (position) => {
     // game stops when the clicked square already has value or winner is decided
-    if(board[position] || winner){
+    if(history[history.length - 1].board[position] || winner){
       return;
     }
 
-    // setting new state of board with callback function
-    // prev->value
-    setBoard((prev)=>{
-      return prev.map((value, idx)=>{
+    //storing game history in state variable -> history
+    setHistory((prev)=>{
+      const last = prev[prev.length - 1];
+
+      const newBoard = last.board.map((value, idx)=>{
         if(idx === position){
-          setisXNext(isXNext?false: true)
-          return isXNext? 'X': 'O';
+          return last.isXNext? 'X': 'O';
         }
         else{
           return value;
         }
       })
-    })
-  }
+      return prev.concat({board: newBoard, isXNext: !last.isXNext});
+    }) ;
 
+    setCurrentMove(prev => prev+1);
+  }
+  console.log(history);
   // Calculate winner by combinations
-  const winner = calculateWinner(board);
+  const winner = calculateWinner(current.board);
+ 
 
   // display message
-  let message = winner? `Winner ${winner}` : `Next Turn ${isXNext? 'X': 'O'}`;
+  let message = winner? `Winner ${winner}` : `Next Turn ${current.isXNext? 'X': 'O'}`;
+  let myStyle = {}
 
+  function moveTo(move){
+    setCurrentMove(move)
+  }
+  
   return (
     <div className="app">
       <h1>Tic Tac Toe</h1>
       <h3>{message}</h3>
-      <Board board={board} handleBoardClick={handleBoardClick} />
+      <Board board={current.board} handleBoardClick={handleBoardClick} />
+      <History history={history} moveTo={moveTo} currentMove={currentMove} />
     </div>
   );
 };
